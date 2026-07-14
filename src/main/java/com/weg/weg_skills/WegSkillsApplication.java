7ab6e2dbb646b04c6361a68f5febbe79136d7aca
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-14 10:50:03 -0300

    feat: boilerplate added

---
+++ src/main/java/com/weg/weg_skills/WegSkillsApplication.java
@@ -0,0 +1,13 @@
+package com.weg.weg_skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegSkillsApplication.class, args);
+	}
+
+}
