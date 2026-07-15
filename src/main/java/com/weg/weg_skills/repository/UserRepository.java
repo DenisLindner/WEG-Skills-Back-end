77ac97bb58264394d718c4bba71a287aa5942368
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-15 10:56:57 -0300

    feat: users repository added

---
+++ src/main/java/com/weg/weg_skills/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.weg.weg_skills.repository;
+
+import com.weg.weg_skills.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
