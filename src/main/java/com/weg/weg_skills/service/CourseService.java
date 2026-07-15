5bc550d148112c4cf2dc404736408ff5d5b9e96c
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-15 14:11:48 -0300

    feat: update course method added in course service

--- src/main/java/com/weg/weg_skills/service/CourseService.java
+++ src/main/java/com/weg/weg_skills/service/CourseService.java
@@ -2,6 +2,7 @@ package com.weg.weg_skills.service;
 
 import com.weg.weg_skills.dto.CourseCreateRequestDTO;
 import com.weg.weg_skills.dto.CourseResponseDTO;
+import com.weg.weg_skills.dto.CourseUpdateRequestDTO;
 import com.weg.weg_skills.mapper.CourseMapper;
 import com.weg.weg_skills.model.Course;
 import com.weg.weg_skills.repository.CourseRepository;
@@ -34,8 +35,27 @@ public class CourseService {
         return courses.stream().map(courseMapper::toResponse).toList();
     }
 
-    public  CourseResponseDTO findById(Long id) {
+    public CourseResponseDTO findById(Long id) {
         Course course = courseRepository.findById(id).orElseThrow(RuntimeException::new);
+
+        return courseMapper.toResponse(course);
+    }
+
+    public CourseResponseDTO update(Long id, CourseUpdateRequestDTO dto) {
+        Course course = courseRepository.findById(id).orElseThrow(RuntimeException::new);
+
+        if (dto.title() != null) {
+            if (!dto.title().equals(course.getTitle()) && courseRepository.existsByTitleIgnoreCase(dto.title())) {
+                throw new RuntimeException();
+            }
+            course.setTitle(dto.title());
+        }
+
+        if (dto.description() != null) {
+            course.setDescription(dto.description());
+        }
+
+        course = courseRepository.save(course);
 
         return courseMapper.toResponse(course);
     }
