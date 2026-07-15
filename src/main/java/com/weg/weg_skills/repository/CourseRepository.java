c873ad741be19a38d843fbb4463517f7c0b5d8cf
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-15 13:41:02 -0300

    fix: exists by course title, variable renamed by "name" to "title" fixed

--- src/main/java/com/weg/weg_skills/repository/CourseRepository.java
+++ src/main/java/com/weg/weg_skills/repository/CourseRepository.java
@@ -4,5 +4,5 @@ import com.weg.weg_skills.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    Boolean existsByTitleIgnoreCase(String name);
+    Boolean existsByTitleIgnoreCase(String title);
 }
