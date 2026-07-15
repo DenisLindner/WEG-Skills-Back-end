6081a8eba664b5877f2c0244eae616f8f6536eac
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-15 13:42:13 -0300

    feat: create course method added in course service

--- src/main/java/com/weg/weg_skills/service/CourseService.java
+++ src/main/java/com/weg/weg_skills/service/CourseService.java
@@ -1,6 +1,9 @@
 package com.weg.weg_skills.service;
 
+import com.weg.weg_skills.dto.CourseCreateRequestDTO;
+import com.weg.weg_skills.dto.CourseResponseDTO;
 import com.weg.weg_skills.mapper.CourseMapper;
+import com.weg.weg_skills.model.Course;
 import com.weg.weg_skills.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,16 @@ import org.springframework.stereotype.Service;
 public class CourseService {
     private CourseRepository courseRepository;
     private CourseMapper courseMapper;
+
+    public CourseResponseDTO create(CourseCreateRequestDTO dto) {
+        if (courseRepository.existsByTitleIgnoreCase(dto.title())) {
+            throw new RuntimeException();
+        }
+
+        Course course = courseMapper.toEntity(dto);
+
+        course = courseRepository.save(course);
+
+        return courseMapper.toResponse(course);
+    }
 }
