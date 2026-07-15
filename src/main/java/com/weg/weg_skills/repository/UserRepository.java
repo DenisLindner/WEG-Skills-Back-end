54b56912afae2f90f05ccfc5ee1095b1f393aa28
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-15 11:16:20 -0300

    feat: exists by email method added in users repository

--- src/main/java/com/weg/weg_skills/repository/UserRepository.java
+++ src/main/java/com/weg/weg_skills/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.weg.weg_skills.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
 }
