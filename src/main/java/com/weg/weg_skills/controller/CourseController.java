b47422e1c76b745cbf84ebf5046617cd6e599461
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-15 14:18:05 -0300

    feat: create course method added in course controller

--- src/main/java/com/weg/weg_skills/controller/CourseController.java
+++ src/main/java/com/weg/weg_skills/controller/CourseController.java
@@ -1,13 +1,24 @@
 package com.weg.weg_skills.controller;
 
+import com.weg.weg_skills.dto.CourseCreateRequestDTO;
+import com.weg.weg_skills.dto.CourseResponseDTO;
 import com.weg.weg_skills.service.CourseService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "courses")
+@RequestMapping(path = "/courses")
 @AllArgsConstructor
 public class CourseController {
     private CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<CourseResponseDTO> create(@RequestBody @Valid CourseCreateRequestDTO dto) {
+        return ResponseEntity.status(201).body(courseService.create(dto));
+    }
 }
