52b872b0d0ea591e9c5e47659652f7da9e29ccd9
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-16 08:49:12 -0300

    feat: method create review added in review controller

--- src/main/java/com/weg/weg_skills/controller/ReviewController.java
+++ src/main/java/com/weg/weg_skills/controller/ReviewController.java
@@ -1,7 +1,13 @@
 package com.weg.weg_skills.controller;
 
+import com.weg.weg_skills.dto.ReviewCreateRequestDTO;
+import com.weg.weg_skills.dto.ReviewResponseDTO;
 import com.weg.weg_skills.service.ReviewService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class ReviewController {
     private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<ReviewResponseDTO> create(@RequestBody @Valid ReviewCreateRequestDTO dto) {
+        return ResponseEntity.status(201).body(reviewService.create(dto));
+    }
 }
