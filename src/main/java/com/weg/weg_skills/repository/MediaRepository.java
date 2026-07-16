832139855fff85e8c16cf8b1482ae1ed55fe3487
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-16 13:02:21 -0300

    feat: media repository added

---
+++ src/main/java/com/weg/weg_skills/repository/MediaRepository.java
@@ -0,0 +1,7 @@
+package com.weg.weg_skills.repository;
+
+import com.weg.weg_skills.model.Media;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaRepository extends JpaRepository<Media, Long> {
+}
