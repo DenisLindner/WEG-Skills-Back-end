78622f597f4241f99335cdcaa16fdff4d1bb65db
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-16 13:04:55 -0300

    feat: create media upload request dto added

---
+++ src/main/java/com/weg/weg_skills/dto/CreateMediaUploadRequestDTO.java
@@ -0,0 +1,18 @@
+package com.weg.weg_skills.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record CreateMediaUploadRequestDTO(
+        @NotBlank(message = "Filename is required")
+        String fileName,
+
+        @NotBlank(message = "ContentType is required")
+        String contentType,
+
+        @NotNull(message = "Size is required")
+        @Positive(message = "Size must be positive")
+        long size
+) {
+}
