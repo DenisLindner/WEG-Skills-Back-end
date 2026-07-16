66fd88091cf4a820f104af9077791758b5b3b7c0
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-16 11:22:47 -0300

    feat: minio config added

---
+++ src/main/java/com/weg/weg_skills/config/MinioConfig.java
@@ -0,0 +1,20 @@
+package com.weg.weg_skills.config;
+
+import io.minio.MinioClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Bean
+    public MinioClient minioClient(MinioProperties minioProperties) {
+        return MinioClient.builder()
+                .endpoint(minioProperties.endpoint())
+                .credentials(
+                        minioProperties.accessKey(),
+                        minioProperties.secretKey()
+                )
+                .build();
+    }
+}
