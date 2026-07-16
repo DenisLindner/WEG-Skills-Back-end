6c127b2aa41f3a0f9f6d13fbd6de800657d1459f
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-16 11:13:13 -0300

    feat: minio properties and dependency added

--- src/main/java/com/weg/weg_skills/WegSkillsApplication.java
+++ src/main/java/com/weg/weg_skills/WegSkillsApplication.java
@@ -1,9 +1,12 @@
 package com.weg.weg_skills;
 
+import com.weg.weg_skills.config.MinioProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MinioProperties.class)
 public class WegSkillsApplication {
 
 	public static void main(String[] args) {
