be21efd70a2d46a5a55369904116b931bf7469ce
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-07-16 10:59:23 -0300

    feat: created the lesson create request dto.

---
+++ src/main/java/com/weg/weg_skills/dto/LessonCreateRequestDTO.java
@@ -0,0 +1,22 @@
+package com.weg.weg_skills.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LessonCreateRequestDTO(
+        @NotBlank(message = "Title is required")
+        @Size(
+                min = 3,
+                max = 128,
+                message = "Title must contain between 3 and 128 characters"
+        )
+        String title,
+
+        @Size(
+                min = 3,
+                max = 255,
+                message = "Description must contain between 3 and 255 characters"
+        )
+        String description
+) {
+}
