2ed033f40b2a1badc40a6bb61436f0f0c21899e7
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-16 08:36:55 -0300

    feat: method find all reviews by course added in review repository

--- src/main/java/com/weg/weg_skills/repository/ReviewRepository.java
+++ src/main/java/com/weg/weg_skills/repository/ReviewRepository.java
@@ -5,7 +5,10 @@ import com.weg.weg_skills.model.Review;
 import com.weg.weg_skills.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Boolean existsByCourseAndUser(Course course, User user);
     Review findByCourseAndUser(Course course, User user);
+    List<Review> findAllByCourse(Course course);
 }
