7bf0e02e8aecd0eaf42be9dd6aca31aef5acd4fc
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-16 08:19:17 -0300

    feat: exists by course and user added in review repository

--- src/main/java/com/weg/weg_skills/repository/ReviewRepository.java
+++ src/main/java/com/weg/weg_skills/repository/ReviewRepository.java
@@ -1,7 +1,10 @@
 package com.weg.weg_skills.repository;
 
+import com.weg.weg_skills.model.Course;
 import com.weg.weg_skills.model.Review;
+import com.weg.weg_skills.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Boolean existsByCourseAndUser(Course course, User user);
 }
