fd075d7bcb472c26658670046226073d4d883f51
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-17 10:44:40 -0300

    fix: removed atribute videoUrl of the lesson response dto

--- src/main/java/com/weg/weg_skills/dto/LessonResponseDTO.java
+++ src/main/java/com/weg/weg_skills/dto/LessonResponseDTO.java
@@ -3,7 +3,6 @@ package com.weg.weg_skills.dto;
 public record LessonResponseDTO(
         Long id,
         String title,
-        String description,
-        String videoUrl
+        String description
 ) {
 }
