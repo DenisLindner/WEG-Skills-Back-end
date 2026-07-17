98357ee22f482c3bbcafa2eace9c288276a3cf27
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-17 14:17:55 -0300

    feat: imageUrl added in module response dto

--- src/main/java/com/weg/weg_skills/dto/ModuleResponseDTO.java
+++ src/main/java/com/weg/weg_skills/dto/ModuleResponseDTO.java
@@ -3,6 +3,7 @@ package com.weg.weg_skills.dto;
 public record ModuleResponseDTO(
         Long id,
         String title,
-        String description
+        String description,
+        String imageUrl
 ) {
 }
