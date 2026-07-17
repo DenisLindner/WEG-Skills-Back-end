8d583eff5f8c4703cdf89d7388bf1ca325bdf811
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-17 14:12:27 -0300

    feat: upload video method added in lesson service

--- src/main/java/com/weg/weg_skills/service/LessonService.java
+++ src/main/java/com/weg/weg_skills/service/LessonService.java
@@ -1,8 +1,6 @@
 package com.weg.weg_skills.service;
 
-import com.weg.weg_skills.dto.LessonCreateRequestDTO;
-import com.weg.weg_skills.dto.LessonResponseDTO;
-import com.weg.weg_skills.dto.LessonUpdateRequestDTO;
+import com.weg.weg_skills.dto.*;
 import com.weg.weg_skills.mapper.LessonMapper;
 import com.weg.weg_skills.model.Lesson;
 import com.weg.weg_skills.model.Module;
@@ -10,6 +8,7 @@ import com.weg.weg_skills.repository.LessonRepository;
 import com.weg.weg_skills.repository.ModuleRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +18,7 @@ public class LessonService {
     private LessonRepository lessonRepository;
     private LessonMapper lessonMapper;
     private ModuleRepository moduleRepository;
+    private MediaService mediaService;
 
     public LessonResponseDTO create(LessonCreateRequestDTO dto) {
         Module module = moduleRepository.findById(dto.moduleId())
@@ -35,6 +35,21 @@ public class LessonService {
         return lessonMapper.toResponse(lesson);
     }
 
+    @Transactional
+    public UploadTicketResponseDTO uploadVideo(Long id, CreateMediaUploadRequestDTO dto) {
+        Lesson lesson = lessonRepository.findById(id).orElseThrow(RuntimeException::new);
+
+        CreatedMediaUpload createdMedia = mediaService.createLessonVideoUpload(lesson.getModule().getCourse().getId(), lesson.getModule().getId(), lesson.getId(), null, dto);
+
+        if (lesson.getVideo() != null) {
+            mediaService.delete(lesson.getVideo().getId());
+        }
+        lesson.setVideo(createdMedia.media());
+        lessonRepository.save(lesson);
+
+        return createdMedia.ticket();
+    }
+
     public List<LessonResponseDTO> findAllByModule(Long moduleId) {
         if (!moduleRepository.existsById(moduleId)) {
             throw new RuntimeException();
@@ -45,10 +60,10 @@ public class LessonService {
         return lessons.stream().map(lessonMapper::toResponse).toList();
     }
 
-    public LessonResponseDTO findById(Long id) {
+    public LessonDetailsResponseDTO findById(Long id) {
         Lesson lesson = lessonRepository.findById(id).orElseThrow(RuntimeException::new);
 
-        return lessonMapper.toResponse(lesson);
+        return lessonMapper.toResponseDetails(lesson, lesson.getVideo() != null && lesson.getVideo().isReady() ? mediaService.getPlaybackVideoUrl(lesson.getVideo().getId()): null);
     }
 
     public LessonResponseDTO update(Long id, LessonUpdateRequestDTO dto) {
