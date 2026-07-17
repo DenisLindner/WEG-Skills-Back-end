25d9a26d4278d7eafc06e59de0153482d00221d3
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-17 13:42:35 -0300

    fix: media controller path fixed

--- src/main/java/com/weg/weg_skills/controller/MediaController.java
+++ src/main/java/com/weg/weg_skills/controller/MediaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.InvalidPropertiesFormatException;
 
 @RestController
-@RequestMapping(path = "medias")
+@RequestMapping(path = "/medias")
 @AllArgsConstructor
 public class MediaController {
     private MediaService mediaService;
