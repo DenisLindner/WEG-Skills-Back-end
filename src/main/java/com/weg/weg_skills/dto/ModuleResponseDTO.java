937c71c03847051ed0fc4c8d1861e55dc6aed489
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-17 10:44:24 -0300

    fix: removed atribute imageUrl of the module response dto

--- src/main/java/com/weg/weg_skills/dto/ModuleResponseDTO.java
+++ src/main/java/com/weg/weg_skills/dto/ModuleResponseDTO.java
@@ -3,7 +3,6 @@ package com.weg.weg_skills.dto;
 public record ModuleResponseDTO(
         Long id,
         String title,
-        String description,
-        String imageUrl
+        String description
 ) {
 }
