6d6ce1e4f531d750e631e019127b35707cf72e79
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-17 10:44:13 -0300

    fix: removed atribute imageUrl of the course response dto

--- src/main/java/com/weg/weg_skills/dto/CourseResponseDTO.java
+++ src/main/java/com/weg/weg_skills/dto/CourseResponseDTO.java
@@ -3,7 +3,6 @@ package com.weg.weg_skills.dto;
 public record CourseResponseDTO(
         Long id,
         String title,
-        String description,
-        String imageUrl
+        String description
 ) {
 }
