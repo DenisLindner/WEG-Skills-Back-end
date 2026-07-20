7eb3e5803362b885d6ff3fc377d304467c8e972e
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-20 12:39:32 -0300

    feat: jwt properties record added

---
+++ src/main/java/com/weg/weg_skills/config/security/JwtProperties.java
@@ -0,0 +1,13 @@
+package com.weg.weg_skills.config.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+@ConfigurationProperties(prefix = "security.jwt")
+public record JwtProperties(
+        String secret,
+        Duration expiration,
+        String issuer
+) {
+}
