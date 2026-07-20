49e2c5b77677eb3669035db042ec8a140139753d
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-20 12:43:20 -0300

    fix: user service fixed

--- src/main/java/com/weg/weg_skills/service/UserService.java
+++ src/main/java/com/weg/weg_skills/service/UserService.java
@@ -1,10 +1,6 @@
 package com.weg.weg_skills.service;
 
-import com.weg.weg_skills.dto.UserCreateRequestDTO;
-import com.weg.weg_skills.dto.UserResponseDTO;
-import com.weg.weg_skills.exceptions.DuplicateResourceException;
 import com.weg.weg_skills.mapper.UserMapper;
-import com.weg.weg_skills.model.User;
 import com.weg.weg_skills.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,16 +10,4 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private UserRepository userRepository;
     private UserMapper userMapper;
-
-    public UserResponseDTO create(UserCreateRequestDTO dto) {
-        if (userRepository.existsByEmail(dto.email())) {
-            throw new DuplicateResourceException("User", "email", dto.email());
-        }
-
-        User user = userMapper.toEntity(dto);
-
-        user = userRepository.save(user);
-
-        return userMapper.toResponse(user);
-    }
 }
