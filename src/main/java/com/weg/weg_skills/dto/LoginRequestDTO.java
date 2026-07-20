e25d2068c973bf5fccd4dff9a267457e66909382
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-20 12:45:30 -0300

    feat: login request dto added

---
+++ src/main/java/com/weg/weg_skills/dto/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package com.weg.weg_skills.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO(
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email must be a valid email")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        String password
+) {
+}
