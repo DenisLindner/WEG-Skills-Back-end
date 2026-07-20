88b491db510b1950fe6bbeda89dd5864a524d992
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-07-20 10:12:55 -0300

    feat: documented the media controller for swagger.

--- src/main/java/com/weg/weg_skills/controller/MediaController.java
+++ src/main/java/com/weg/weg_skills/controller/MediaController.java
@@ -8,14 +8,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping(path = "/medias")
 @AllArgsConstructor
+@Tag(name = "Mídias", description = "Endpoints para gerenciamento de mídias e arquivos")
 public class MediaController {
     private MediaService mediaService;
 
     @PostMapping(path = "/{mediaId}/complete")
+    @Operation(summary = "Finaliza e confirma o upload de uma mídia")
     public ResponseEntity<MediaResponseDTO> completeUpload(@PathVariable Long mediaId) {
         return ResponseEntity.status(200).body(mediaService.completeUpload(mediaId));
     }
