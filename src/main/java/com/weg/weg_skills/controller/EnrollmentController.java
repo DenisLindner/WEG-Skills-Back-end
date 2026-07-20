6dbb804f43a68c8a7ece86c3859e77f708165038
Bruno Gustavo Kannenberg <brunokannenberg794@gmail.com>
2026-07-20 10:08:37 -0300

    feat: documented the enrollment controller for swagger.

--- src/main/java/com/weg/weg_skills/controller/EnrollmentController.java
+++ src/main/java/com/weg/weg_skills/controller/EnrollmentController.java
@@ -10,14 +10,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/enrollments")
 @AllArgsConstructor
+@Tag(name = "Matrículas", description = "Endpoints para gerenciamento de matrículas nos cursos")
 public class EnrollmentController {
     private final EnrollmentService enrollmentService;
 
     @PostMapping
+    @Operation(summary = "Realiza a matrícula de um usuário em um curso")
     public ResponseEntity<EnrollmentResponseDTO> enroll(@Valid @RequestBody EnrollmentRequestDTO requestDTO) {
         return ResponseEntity.status(201).body(enrollmentService.enrollUser(requestDTO));
     }
