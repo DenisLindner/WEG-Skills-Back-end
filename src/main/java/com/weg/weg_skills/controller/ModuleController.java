d4da1864928fb4559658f8bf1d96daeef37ca070
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-21 07:37:59 -0300

    feat: current user added in methods of module controller

--- src/main/java/com/weg/weg_skills/controller/ModuleController.java
+++ src/main/java/com/weg/weg_skills/controller/ModuleController.java
@@ -5,6 +5,8 @@ import com.weg.weg_skills.service.ModuleService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,8 +28,8 @@ public class ModuleController {
 
     @PostMapping(path = "/{id}/images/upload")
     @Operation(summary = "Generates a ticket for uploading the module image")
-    public ResponseEntity<UploadTicketResponseDTO> uploadImage(@PathVariable Long id, @RequestBody @Valid CreateMediaUploadRequestDTO dto) {
-        return ResponseEntity.status(201).body(moduleService.uploadImage(id, dto));
+    public ResponseEntity<UploadTicketResponseDTO> uploadImage(@PathVariable Long id, @RequestBody @Valid CreateMediaUploadRequestDTO dto, @AuthenticationPrincipal Jwt jwt) {
+        return ResponseEntity.status(201).body(moduleService.uploadImage(id, dto, jwt.getClaim("userId")));
     }
 
     @GetMapping(path = "/course/{courseId}")
