856f33b51aa3210b9293cecf935fbd6db1de3e56
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-22 07:51:51 -0300

    fix: unused import removed

--- src/main/java/com/weg/weg_skills/controller/LessonController.java
+++ src/main/java/com/weg/weg_skills/controller/LessonController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(path = "/lessons")
 @AllArgsConstructor
