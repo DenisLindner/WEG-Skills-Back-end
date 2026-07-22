5a09745790f7d7ffb08662c1a17749499281dae0
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-22 07:44:08 -0300

    feat: findAll by user with pagination added in enrollment repository

--- src/main/java/com/weg/weg_skills/repository/EnrollmentRepository.java
+++ src/main/java/com/weg/weg_skills/repository/EnrollmentRepository.java
@@ -3,9 +3,12 @@ package com.weg.weg_skills.repository;
 import com.weg.weg_skills.model.Course;
 import com.weg.weg_skills.model.Enrollment;
 import com.weg.weg_skills.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     boolean existsByUserAndCourse(User user, Course course);
     boolean existsByUserIdAndCourse(Long userId, Course course);
+    Page<Enrollment> findAllByUserOrderByCreatedAtDesc(User user, Pageable pageable);
 }
