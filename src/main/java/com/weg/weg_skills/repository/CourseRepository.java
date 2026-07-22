2dd65389aa57ab48177af85cef9eb45ba4dffaf3
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-22 07:43:50 -0300

    feat: findAll with pagination added in course repository

--- src/main/java/com/weg/weg_skills/repository/CourseRepository.java
+++ src/main/java/com/weg/weg_skills/repository/CourseRepository.java
@@ -1,8 +1,11 @@
 package com.weg.weg_skills.repository;
 
 import com.weg.weg_skills.model.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     Boolean existsByTitleIgnoreCase(String title);
+    Page<Course> findAllOrderByCreatedAtDesc(Pageable pageable);
 }
