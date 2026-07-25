88e48c5e45d183ce462cca074e79513934a861c0
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-25 10:14:52 -0300

    feat: find most enrollments courses added in course repository

--- src/main/java/com/weg/weg_skills/repository/CourseRepository.java
+++ src/main/java/com/weg/weg_skills/repository/CourseRepository.java
@@ -1,8 +1,20 @@
 package com.weg.weg_skills.repository;
 
 import com.weg.weg_skills.model.Course;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     Boolean existsByTitleIgnoreCase(String title);
+    @Query("""
+            SELECT c
+            FROM Course c
+            LEFT JOIN Enrollment e
+            GROUP BY c
+            ORDER BY COUNT(e) DESC
+        """)
+    List<Course> findMostEnrollmentsCourses(Pageable pageable);
 }
