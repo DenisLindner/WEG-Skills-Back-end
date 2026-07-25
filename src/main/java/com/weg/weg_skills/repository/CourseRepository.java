11cde94d5e426122edb2f1299b1affdfe6bfe749
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-25 10:16:12 -0300

    feat: find all by name added in course repository

--- src/main/java/com/weg/weg_skills/repository/CourseRepository.java
+++ src/main/java/com/weg/weg_skills/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package com.weg.weg_skills.repository;
 
 import com.weg.weg_skills.model.Course;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,5 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
             ORDER BY COUNT(e) DESC
         """)
     List<Course> findMostEnrollmentsCourses(Pageable pageable);
+    Page<Course> findAllByTitleIgnoreCaseOrderByCreatedAtDesc(String title, Pageable pageable);
 }
