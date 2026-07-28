012adb84c3db1b16ea7545aeffdfa11a205e0f88
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-27 23:15:59 -0300

    feat: entity graph added in review repository

--- src/main/java/com/weg/weg_skills/repository/ReviewRepository.java
+++ src/main/java/com/weg/weg_skills/repository/ReviewRepository.java
@@ -5,11 +5,15 @@ import com.weg.weg_skills.model.Review;
 import com.weg.weg_skills.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Boolean existsByCourseAndUser(Course course, User user);
     Boolean existsByIdAndUserId(Long id, Long userId);
+    @EntityGraph(attributePaths = {"course", "user", "user.image"})
     Page<Review> findAllByCourseId(Long courseId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"course", "user", "user.image"})
     Page<Review> findAllByUserId(Long userId, Pageable pageable);
 }
