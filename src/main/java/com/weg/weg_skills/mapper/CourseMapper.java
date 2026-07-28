8da23020dc4abeb5d684e00deef4a52128b4f0cd
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-28 10:52:01 -0300

    feat: to response progress method added in course mapper

--- src/main/java/com/weg/weg_skills/mapper/CourseMapper.java
+++ src/main/java/com/weg/weg_skills/mapper/CourseMapper.java
@@ -1,11 +1,16 @@
 package com.weg.weg_skills.mapper;
 
+import com.weg.weg_skills.dto.CourseProgressResponseDTO;
 import com.weg.weg_skills.dto.CourseResponseDTO;
 import com.weg.weg_skills.dto.CourseWithRatingResponseDTO;
+import com.weg.weg_skills.dto.LessonProgressResponseDTO;
 import com.weg.weg_skills.model.Course;
 import com.weg.weg_skills.model.User;
 import com.weg.weg_skills.projection.CourseWithRatingProjection;
+import com.weg.weg_skills.projection.ProgressProjection;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CourseMapper {
@@ -19,5 +24,9 @@ public class CourseMapper {
 
     public CourseWithRatingResponseDTO toResponseProjection(CourseWithRatingProjection projection, String url) {
         return new CourseWithRatingResponseDTO(projection.getId(), projection.getTitle(), projection.getDescription(), projection.getRating(), url);
+    }
+
+    public CourseProgressResponseDTO toResponseProgress(ProgressProjection projection, List<LessonProgressResponseDTO> lessons) {
+        return new CourseProgressResponseDTO(projection.getCourseId(), projection.getCompletedLessons(), projection.getTotalLessons(), projection.getPercentage(), lessons);
     }
 }
