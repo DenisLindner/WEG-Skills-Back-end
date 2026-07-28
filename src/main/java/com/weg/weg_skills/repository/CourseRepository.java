2b95a3e106616520aa81b66079c73ba2b782c2cd
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-28 10:51:36 -0300

    feat: find progress by userId method added in course repository

--- src/main/java/com/weg/weg_skills/repository/CourseRepository.java
+++ src/main/java/com/weg/weg_skills/repository/CourseRepository.java
@@ -2,14 +2,17 @@ package com.weg.weg_skills.repository;
 
 import com.weg.weg_skills.model.Course;
 import com.weg.weg_skills.projection.CourseWithRatingProjection;
+import com.weg.weg_skills.projection.ProgressProjection;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     Boolean existsByTitleIgnoreCase(String title);
@@ -35,4 +38,21 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     List<CourseWithRatingProjection> findMostEnrollmentsCourses(Pageable pageable);
     @EntityGraph(attributePaths = "image")
     Page<Course> findAllByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+    @Query("""
+            SELECT
+                c.id AS courseId,
+                COUNT(DISTINCT p.id) AS completedLessons,
+                COUNT(DISTINCT l.id) AS totalLessons
+            FROM Course c
+            JOIN c.enrollments e
+            LEFT JOIN c.modules m
+            LEFT JOIN m.lessons l
+            LEFT JOIN l.lessonProgresses p
+                ON p.enrollment = e
+            WHERE c.id = :courseId
+              AND e.user.id = :userId
+            GROUP BY c.id
+        """)
+    Optional<ProgressProjection> findProgressByUserId(@Param("userId") Long userId, @Param("courseId") Long courseId);
 }
