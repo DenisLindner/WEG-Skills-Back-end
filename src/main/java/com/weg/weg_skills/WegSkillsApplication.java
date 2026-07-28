4db0d49afde6cad24f4dfa4074a4e7a0dc588136
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-27 23:14:03 -0300

    fix: enable caching annotation removed in application

--- src/main/java/com/weg/weg_skills/WegSkillsApplication.java
+++ src/main/java/com/weg/weg_skills/WegSkillsApplication.java
@@ -4,13 +4,11 @@ import com.weg.weg_skills.config.minio.MinioProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(MinioProperties.class)
 @EnableScheduling
-@EnableCaching
 public class WegSkillsApplication {
 
 	public static void main(String[] args) {
