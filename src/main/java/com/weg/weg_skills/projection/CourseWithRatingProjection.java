370c9616d732c9407b79a9d1fec98cc986af03f4
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-28 23:31:54 -0300

    feat: course status atribute added in course with rating projection

--- src/main/java/com/weg/weg_skills/projection/CourseWithRatingProjection.java
+++ src/main/java/com/weg/weg_skills/projection/CourseWithRatingProjection.java
@@ -1,11 +1,13 @@
 package com.weg.weg_skills.projection;
 
+import com.weg.weg_skills.enums.CourseStatus;
 import com.weg.weg_skills.model.Media;
 
 public interface CourseWithRatingProjection {
     Long getId();
     String getTitle();
     String getDescription();
+    CourseStatus getCourseStatus();
     Double getRating();
     Media getImage();
 }
